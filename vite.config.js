e91c061412ba5d6f0488065e22cc44e0a9822f90
unknown <fpemadeira@gmail.com>
2025-04-28 19:43:42 -0300

    chore: install AdminLTE template

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss',
+            input: ['resources/scss/app.scss',
                     'resources/js/app.js'],
             refresh: true,
         }),
