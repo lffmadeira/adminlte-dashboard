703bce1c31b15beed10958d973775e160e6239b2
unknown <fpemadeira@gmail.com>
2025-05-01 16:40:38 -0300

    style: enhance dashboard visuals with icons/images

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
+import.meta.glob([
+    '../images/**',
+    '../fonts/**'
+  ]);
 import "/node_modules/bootstrap/dist/js/bootstrap.esm.js";
 import "admin-lte/dist/js/adminlte.min.js";
 import "/resources/js/overlayscrollbar.js";
